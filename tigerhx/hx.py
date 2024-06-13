5e7eaeb8b85bbc4c256d79dcce8ed02f719dce2d
htylab <10368378+htylab@users.noreply.github.com>
2024-06-14 06:38:51 +0800

    Update hx.py

--- tigerhx/hx.py
+++ tigerhx/hx.py
@@ -105,7 +105,8 @@ def run_args(args):
     print('Total nii files:', len(input_file_list))
 
     if args.model is None:
-        args.model = 'cine4d_xyz_v002_m12ac'
+        #args.model = 'cine4d_xyz_v002_m12ac'
+        args.model = cine4d_v0003_xy_mms12acdc.onnx
         #args.model = 'cine4d_xyz_v001_m12.onnx'
 
     model_name = lib_tool.get_model(args.model)
