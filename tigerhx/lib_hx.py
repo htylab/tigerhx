03172ccf1bbde3323bfba413058eb5cc467ee884
htylab <10368378+htylab@users.noreply.github.com>
2024-06-20 12:39:58 +0800

    Update lib_hx.py

--- tigerhx/lib_hx.py
+++ tigerhx/lib_hx.py
@@ -35,9 +35,8 @@ def post(mask, th=50):
     masknew = mask * 0
     for jj in range(1, int(mask.max()) + 1):
         masknew[getLarea(mask == jj)] = jj
-    print(th)
     if (np.sum(masknew==1) == 0) or (np.sum(masknew>0) < th) :
-        print('XXX       ')
+
         masknew = np.zeros_like(mask, dtype=int)
 
     return masknew
